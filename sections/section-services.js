64ef635861a8156f114584450ff650adbc4d388c
MohJeha-Artist <mohjeha.artist@gmail.com>
2025-12-01 15:54:39 +0200

    Update section-services.js

--- sections/section-services.js
+++ sections/section-services.js
@@ -1,6 +1,6 @@
 class SectionServices extends HTMLElement {
     connectedCallback() {
-      fetch("../sections/section-services.html")
+      fetch("sections/section-services.html")
         .then(res => res.text())
         .then(html => {
           this.innerHTML = html;
